15ac5559f1f2a43a9e4f5e4807f79ef1383b9c84
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-05-09 08:27:15 +0300

    changes js functions to python funtions

--- lib/functions.py
+++ lib/functions.py
@@ -2,15 +2,23 @@
 
 def greet_programmer():
     pass
+    print("Hello, programmer!")
 
 def greet(name):
     pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
     pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
     pass
+    return num1 + num2
 
 def halve(number):
     pass
+    if (type(number) == int or type(number) == float):
+        return number / 2
+    else:
+        return None
